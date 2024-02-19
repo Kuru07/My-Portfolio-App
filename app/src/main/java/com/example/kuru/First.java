ea02bbff337680ab0c4b428dd4ee3be3028f5652
Kuru07 <kumarakuru7007@gmail.com>
2024-02-19 18:53:12 +0530

    Final Commit

--- app/src/main/java/com/example/kuru/First.java
+++ app/src/main/java/com/example/kuru/First.java
@@ -6,24 +6,24 @@ import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
-            public class First extends AppCompatActivity {
+public class First extends AppCompatActivity {
 
-                @Override
-                protected void onCreate(Bundle savedInstanceState) {
-                    super.onCreate(savedInstanceState);
-                    setContentView(R.layout.activity_main);
-                }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-                public void onLoginClick(View view) {
-                    // Intent to start LoginActivity
-                    Intent intent = new Intent(this, Login.class);
-                    startActivity(intent);
-                }
+    public void onLoginClick(View view) {
+        // Intent to start LoginActivity
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
 
-                public void onSignUpClick(View view) {
-                    // Intent to start SignUpActivity
-                    Intent intent = new Intent(this, SignUpActivity.class);
-                    startActivity(intent);
-                }
-            }
+    public void onSignUpClick(View view) {
+        // Intent to start SignUpActivity
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}
 
